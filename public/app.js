94cf14098365fa9b24c8ce3da53a3c4e8a7c71d4
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2026-01-03 23:13:56 +0700

    update button file

--- public/app.js
+++ public/app.js
@@ -382,7 +382,6 @@ function render() {
                                 <input type="text" id="messageInput" placeholder="${t('chat.messagePlaceholder', lang)}" onkeypress="handleKeyPress(event)">
                                 <div class="file-input-wrapper">
                                     <input type="file" id="fileInput" onchange="handleFileSelect(event)">
-                                    <label for="fileInput" class="file-input-label"><i class="fas fa-paperclip"></i> ${t('common.file', lang)}</label>
                                 </div>
                                 <button class="btn btn-primary" onclick="sendMessage()" title="${t('common.send', lang)}"><i class="fas fa-paper-plane"></i></button>
                             </div>
